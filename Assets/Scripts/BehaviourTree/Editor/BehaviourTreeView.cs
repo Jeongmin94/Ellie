d43f05a178a72dafd3ce38bc885e8c8784bea8c8
djmaxjoaa <cpsn6237@gmail.com>
2023-10-11 16:24:24 +0900

    #ELLIE-59 : 유니티 기능 UI Builder으로 GUI 인터페이스 구성 완료 행동트리 기능 구현 예정

--- Assets/Scripts/BehaviourTree/Editor/BehaviourTreeView.cs
+++ Assets/Scripts/BehaviourTree/Editor/BehaviourTreeView.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UIElements;
 using UnityEditor.Experimental.GraphView;
+using UnityEditor;
 
 public class BehaviourTreeView : GraphView
 {
@@ -9,6 +10,16 @@ public class BehaviourTreeView : GraphView
 
     public BehaviourTreeView()
     {
+        // 그리드 백그라운드 생성
+        Insert(0, new GridBackground());
 
+        // 4가지 조작기 추가하기
+        this.AddManipulator(new ContentZoomer());           // 확대, 축소
+        this.AddManipulator(new ContentDragger());          // 그래프 창을 이동
+        this.AddManipulator(new SelectionDragger());        // 노드 선택 드래그
+        this.AddManipulator(new RectangleSelector());       // 다중 선택 드래그
+
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Scripts/BehaviourTree/Editor/BehaviourTreeEditor.uss");
+        styleSheets.Add(styleSheet);
     }
 }
