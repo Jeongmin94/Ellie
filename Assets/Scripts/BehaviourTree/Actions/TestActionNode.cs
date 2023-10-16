2d86f5a01762067f1264375c9df317b8adc4ed9a
djmaxjoaa <cpsn6237@gmail.com>
2023-10-16 11:06:09 +0900

    #ELLIE59 : thekiwicoder behaviour tree 에셋 설치

---
+++ Assets/Scripts/BehaviourTree/Actions/TestActionNode.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheKiwiCoder;
+
+[System.Serializable]
+public class TestActionNode : ActionNode
+{
+    public NodeProperty<int> moveSpeed;
+    public NodeProperty<string> logString;
+
+    protected override void OnStart() {
+    }
+
+    protected override void OnStop() {
+    }
+
+    protected override State OnUpdate() {
+        int currentSpeed = moveSpeed.Value;
+        currentSpeed += 1;
+        logString.Value = currentSpeed.ToString();
+        moveSpeed.Value = currentSpeed;
+
+        return State.Success;
+    }
+}
