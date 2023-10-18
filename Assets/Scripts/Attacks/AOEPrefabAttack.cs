660fd97ae70bcf020e82c9c7cb6fde895916310c
jeongmin <extraking@naver.com>
2023-10-18 10:49:59 +0900

    #release/Sprint231018: 누락된 enemy 관련 코드들 추가 중

---
+++ Assets/Scripts/Attacks/AOEPrefabAttack.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+
+public class AOEPrefabAttack : AbstractAttack
+{
+    [SerializeField] private AOE prefabObject;
+    private Vector3 position;
+    private Vector3 offset;
+    private float damageInterval;
+
+    public override void InitializeAOE
+        (float attackValue, float durationTime, float attackInterval, float attackRange, float damageInterval, GameObject prefabObject)
+    {
+        InitializedBase(attackValue, durationTime, attackInterval, attackRange);
+        Debug.Log("ParameterOBJ : " + prefabObject);
+        this.damageInterval = damageInterval;
+        this.prefabObject = prefabObject.GetComponent<AOE>();
+        if (prefabObject == null)
+            Debug.Log("[AOEPrefabAttack] PrefabObject is Null");
+    }
+
+    public override void ActivateAttack()
+    {
+        AOE obj = Instantiate(prefabObject, position + offset, transform.rotation);
+        obj.SetPrefabData(attackValue, durationTime, damageInterval, gameObject.tag.ToString());
+        StartCoroutine(StartAttackReadyCount());
+    }
+    private IEnumerator StartAttackReadyCount()
+    {
+        IsAttackReady = false;
+        yield return new WaitForSeconds(AttackInterval);
+        IsAttackReady = true;
+    }
+
+    public void SetPrefabPosition(Vector3 position, Vector3 offset)
+    {
+        this.position = position;
+        this.offset = offset;
+    }
+}
---
+++ Assets/Scripts/Characters/WeaponMonster.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponMonster : AbstractMonster
+{
+    //For Test
+    public GameObject player;
+
+    Structures.MonsterStat stat = new()
+    {
+        HP = 10.0f,
+        movementSpeed = 2.0f,
+        rotationSpeed = 3.0f,
+        detectPlayerDistance = 10.0f,
+        overtravelDistance = 15.0f
+    };
+
+    private enum SkillName {WeaponAttack, End}
+    [SerializeField] private GameObject weapon;
+
+    private void OnEnable()
+    {
+        animator = GetComponent<Animator>();
+        //InitializeStat(10, 2, 10, 30, 40);
+        InitializeStat(stat);
+
+        skills = new AbstractAttack[(int)SkillName.End];
+        skills[(int)SkillName.WeaponAttack] = AddSkill(SkillName.WeaponAttack.ToString(), Enums.AttackSkill.WeaponAttack);
+        skills[(int)SkillName.WeaponAttack].InitializeWeapon(2.2f, 1.0f, 3.0f, 3.0f, weapon);
+    }    
+    private void FixedUpdate()
+    {
+        playerDistance = Vector3.Distance(player.transform.position, transform.position);
+        if(!isAttacking)
+        {
+            StartCoroutine(UpdateAttackable());
+        }
+    }
+    private IEnumerator UpdateAttackable()
+    {
+        for(int i=0; i<(int)SkillName.End;i++)
+        {
+            if (playerDistance < skills[i].AttackRange && skills[i].IsAttackReady)
+            {
+                isAttacking = true;
+                animator.SetTrigger("WeaponAttackAnimation");
+                yield return new WaitForSeconds(1.5f);
+                skills[i].ActivateAttack();
+            }
+        }
+        if (isAttacking)
+            StartCoroutine(SetAttackingFalse());
+    }
+    private IEnumerator SetAttackingFalse()
+    {
+        isAttacking = false;
+        yield return new WaitForSeconds(1.5f);
+    }
+
+}
