f6e13a79a3717a60a00b1d3e206a275eafebe766
clairhardt98 <woojae0107@gmail.com>
2023-10-31 15:08:58 +0900

    #ELLIE-238 : 돌맹이가 공격할 수 있도록 ICombatant및 공격 로직 추가

--- Assets/Scripts/Item/Stone/TestStoneLootable.cs
+++ Assets/Scripts/Item/Stone/TestStoneLootable.cs
@@ -1,4 +1,5 @@
 ﻿using Assets.Scripts.Combat;
+using Assets.Scripts.Player;
 using Assets.Scripts.Utils;
 using Channels.Combat;
 using Channels.Components;
@@ -34,13 +35,24 @@ namespace Assets.Scripts.Item.Stone
             ICombatant enemy = collision.gameObject.GetComponent<ICombatant>();
             if (enemy != null)
             {
-
+                Attack(GenerateStonePayload(collision.transform));
             }
         }
 
-        private CombatPayload GenerateStonePayload()
+        private CombatPayload GenerateStonePayload(Transform defender)
         {
             var payload = new CombatPayload();
+            // !TODO : 돌맹이 데이터 읽어와서 현재 돌맹이에 맞는 값으로 페이로드 초기화
+
+            //=> test
+            payload.Type = CombatType.Melee;
+            payload.Attacker = transform;
+            payload.Defender = defender;
+            payload.AttackDirection = Vector3.zero;
+            payload.AttackStartPosition = transform.position;
+            payload.AttackPosition = defender.position;
+            payload.Damage = 5;
+            //<=
             return payload;
         }
     }
