93712221bf933e4d3f9ce2fce5d81e76714c6969
clairhardt98 <woojae0107@gmail.com>
2023-10-31 14:31:47 +0900

    #ELLIE-238 : 플레이어 및 몬스터 전투 작업중

--- Assets/Scripts/Item/Stone/TestStoneLootable.cs
+++ Assets/Scripts/Item/Stone/TestStoneLootable.cs
@@ -1,15 +1,47 @@
-﻿using Assets.Scripts.Managers;
-using Assets.Scripts.Player;
+﻿using Assets.Scripts.Combat;
+using Assets.Scripts.Utils;
+using Channels.Combat;
+using Channels.Components;
+using Channels.Type;
 using UnityEngine;
 namespace Assets.Scripts.Item.Stone
 {
-    public class TestStoneLootable : BaseStone, ILootable
+    public class TestStoneLootable : BaseStone, ICombatant
     {
-        public void Visit(PlayerLooting player)
+        private TicketMachine ticketMachine;
+        private void Awake()
         {
-            Debug.Log("Player Loot : " + this.name);
-            PoolManager.Instance.Push(this);
+            SetTicketMachine();
+        }
 
+        private void SetTicketMachine()
+        {
+            ticketMachine = gameObject.GetOrAddComponent<TicketMachine>();
+            ticketMachine.AddTickets(ChannelType.Combat);
+        }
+        public void Attack(IBaseEventPayload payload)
+        { 
+            ticketMachine.SendMessage(ChannelType.Combat, payload);
+        }
+
+        public void ReceiveDamage(IBaseEventPayload payload)
+        {
+
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            ICombatant enemy = collision.gameObject.GetComponent<ICombatant>();
+            if (enemy != null)
+            {
+
+            }
+        }
+
+        private CombatPayload GenerateStonePayload()
+        {
+            var payload = new CombatPayload();
+            return payload;
         }
     }
 }
