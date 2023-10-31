03b2ccc7ddc8f605b3321383d60c0f22f92c02a0
clairhardt98 <woojae0107@gmail.com>
2023-10-31 09:21:58 +0900

    #ELLIE-238 : 돌맹이 습득 로직 구현
    1. visitor 패턴 구현

---
+++ Assets/Scripts/Item/Stone/TestStoneLootable.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Scripts.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Item.Stone
+{
+    public class TestStoneLootable : BaseStone, ILootable
+    {
+        public void Visit(PlayerLooting player)
+        {
+            //ObjectPoolManager.
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerLooting.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts.Item;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class PlayerLooting : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision collision)
+        {
+            ILootable lootable = collision.gameObject.GetComponent<ILootable>();
+            if (lootable != null)
+            {
+                Accept(lootable);
+            }
+        }
+        private void Accept(ILootable item)
+        {
+            item.Visit(this);
+        }
+    }
+}
