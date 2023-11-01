f76964052a83afc5870f4c9c9772b2e8ce0a382f
djmaxjoaa <cpsn6237@gmail.com>
2023-11-01 16:50:16 +0900

    #ELLIE-210 : 행동트리 서브 트리로 정리 ver1
    1 루트 트리는 조건만 체크 (condition), 서브 트리에서 세부 동작
    2. 아직 블랙보드 변수 정리는 안함

--- Assets/Scripts/Boss/Terrapupa/TerrapupaController.cs
+++ Assets/Scripts/Boss/Terrapupa/TerrapupaController.cs
@@ -15,6 +15,7 @@ namespace Assets.Scripts.Boss.Terrapupa
 
     public class TerrapupaController : MonoBehaviour
     {
+        [SerializeField] private List<BehaviourTree> behaviourTreeList = new List<BehaviourTree>();
         [SerializeField] private BehaviourTreeInstance behaviourTreeInstance;
         [SerializeField] private TerrapupaDataInfo data;
 
