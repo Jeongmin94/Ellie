282bc9a225d48b271d19cbcecceb6a81ece37654
clairhardt98 <woojae0107@gmail.com>
2023-11-06 21:00:07 +0900

    #ELLIE-241 : NPC 및 퀘스트 시스템 작업중

---
+++ Assets/Scripts/NPCs/BaseNPC.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Scripts.InteractiveObjects;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.NPCs
+{
+    public class BaseNPC : MonoBehaviour, IInteractiveObject
+    {
+        List<string>[] dialogList;
+        public void Interact(GameObject obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected virtual void Init()
+        {
+            
+        }
+        // !TODO
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerQuest.cs
@@ -0,0 +1,45 @@
+﻿using Assets.Scripts.Data.Quest;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class PlayerQuest : MonoBehaviour
+    {
+        //스크립터블 오브젝트
+        [SerializeField] private QuestDataList QuestDataListObj;
+
+
+        private void Start()
+        {
+            InitQuestDataList();
+        }
+
+        private void InitQuestDataList()
+        {
+            // !TODO : 구글 스프레드 시트에서 데이터를 파싱하여 List를 갱신
+            foreach(QuestData data in QuestDataListObj.questDataList)
+            {
+                data.status = QuestStatus.CantAccept;
+            }
+            //첫 퀘스트는 수락 가능 상태로 변경
+            QuestDataListObj.questDataList[0].status = QuestStatus.Unaccepted;
+        }
+
+        public void RenewQuestStatus(int questIdx, QuestStatus status)
+        {
+            QuestDataListObj.questDataList[questIdx].status = status;
+        }
+
+        public QuestStatus GetQuestStatus(int questIdx)
+        {
+            return QuestDataListObj.questDataList[questIdx].status;
+        }
+
+        public List<string>[] GetDialog(int questIdx)
+        {
+            return QuestDataListObj.questDataList[questIdx].dialogList;
+        }
+    }
+}
