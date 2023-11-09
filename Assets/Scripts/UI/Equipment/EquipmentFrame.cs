c0f2417d8bd260f2fa96d6a8d762eadaf9e3e784
jeongmin <extraking@naver.com>
2023-11-09 11:17:22 +0900

    #ELLIE-191: 장착 ui 레이아웃 추가

---
+++ Assets/Scripts/UI/Equipment/EquipmentFrame.cs
@@ -0,0 +1,48 @@
+using Assets.Scripts.UI.Framework;
+using Assets.Scripts.UI.Framework.Presets;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI.Equipment
+{
+    public class EquipmentFrame : UIBase
+    {
+        public static readonly string Path = "Slot/Equipment/EquipmentFrame";
+
+        private Image frameImage;
+        private RectTransform imageRect;
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        protected override void Init()
+        {
+            Bind();
+            InitObjects();
+        }
+
+        private void Bind()
+        {
+            frameImage = GetComponent<Image>();
+            imageRect = frameImage.GetComponent<RectTransform>();
+        }
+
+        private void InitObjects()
+        {
+        }
+
+        public void SetFrame(float width, float height)
+        {
+            AnchorPresets.SetAnchorPreset(imageRect, AnchorPresets.MiddleCenter);
+            imageRect.sizeDelta = new Vector2(width, height);
+            imageRect.localPosition = Vector2.zero;
+        }
+
+        public void SetFrameSprite(Sprite sprite)
+        {
+            frameImage.sprite = sprite;
+        }
+    }
+}
