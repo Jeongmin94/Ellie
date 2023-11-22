eb405e669412169bce21b745e145b1b53a353b01
clairhardt98 <woojae0107@gmail.com>
2023-11-22 21:57:57 +0900

    #ELLIE-253 : NPC시스템 작업중

---
+++ Assets/Scripts/InteractiveObjects/NPC/TalkingSkullEldestNPC.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.InteractiveObjects.NPC
+{
+    public class TalkingSkullEldestNPC : BaseNPC
+    {
+        public override void Interact(GameObject obj)
+        {
+            base.Interact(obj);
+            //첫 째가 상호작용할 때 로직
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerQuest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class PlayerQuest : MonoBehaviour
+    {
+        PlayerController controller;
+        // Use this for initialization
+        void Start()
+        {
+            controller = GetComponent<PlayerController>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void StartConversation()
+        {
+            controller.StartConversation();
+        }
+        public void EndConversation()
+        {
+            controller.EndConversation();
+        }
+    }
+}
