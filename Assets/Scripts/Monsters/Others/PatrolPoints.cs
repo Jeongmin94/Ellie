0470fb4cc2e848d8e5288e8b9f980cfbfe36116a
onedeo <iamonedeo@gmail.com>
2023-11-22 16:00:57 +0900

    #ELLIE-96 일반몬스터 헤드샷 추가, 정찰 로직 수정

--- Assets/Scripts/Monsters/Others/PatrolPoints.cs
+++ Assets/Scripts/Monsters/Others/PatrolPoints.cs
@@ -4,22 +4,18 @@ using UnityEngine;
 
 public class PatrolPoints : MonoBehaviour
 {
-    public List<GameObject> patrolPoints;
-
     public Vector3[] GetPatrolPointst()
     {
-        Vector3[] patrolVectors = new Vector3[patrolPoints.Count];
-        for(int i=0; i<patrolPoints.Count;i++)
+        Vector3[] patrolVectors = new Vector3[transform.childCount];
+        int i = 0; 
+        foreach (Transform child in gameObject.transform)
         {
-            patrolVectors[i] = patrolPoints[i].transform.position;
+            patrolVectors[i] = child.transform.position;
             patrolVectors[i].y = transform.position.y;
+            i++;
         }
 
         return patrolVectors;
-    }
-    public List<GameObject>GetPatrolPoints()
-    {
-        return patrolPoints;
     }
 
 }
