ae1adaabecd031b930f0bedc6f9d52494b11c637
onedeo <iamonedeo@gmail.com>
2023-11-28 15:42:32 +0900

    #ELLIE-102 Scene Load 테스트

--- Assets/Scripts/Loading/LoadingUI.cs
+++ Assets/Scripts/Loading/LoadingUI.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Assets.Scripts.Centers.Test;
 using Assets.Scripts.Loading;
 using Assets.Scripts.Managers;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Assets.Scripts.Centers.Test;
+using UnityEditor.SearchService;
 
 public class LoadingUI : MonoBehaviour
 {
@@ -16,19 +20,21 @@ public class LoadingUI : MonoBehaviour
 
     [SerializeField] private TipsParshingInfo data;
 
+    const int loadingDataQuantity = 2;
+    const float spareTimeToLoad = 1.0f;
+
+    const float generalBarSpeed = 0.001f;
+    const float fasterBarSpeed = 0.005f;
+
+    private float barSpeed;
+
     const int imageQuantity = 4;
     const int tipQuantity = 3;
 
-    bool isLoad = false;
-
-    void Update()
+    private void Start()
     {
-        loadingSlider.value = AsyncLoadManager.Instance.GetLoadingProgress();
-        if (!isLoad)
-        {
-            UpdateImageTip();
-            isLoad = true;
-        }
+        UpdateImageTip();
+        StartCoroutine(LoadLevelAsync(TestCenterWithScene.Instance.CurrentScene));
     }
 
     void UpdateImageTip()
@@ -37,5 +43,64 @@ public class LoadingUI : MonoBehaviour
         loadingImage.sprite = ResourceManager.Instance.LoadSprite(imagePath);
         string tip = data.datas[Random.Range(0, tipQuantity)].tip;
         tipText.text = tip;
+    }
+
+    private IEnumerator LoadLevelAsync(SceneName scene)
+    {
+        barSpeed = generalBarSpeed;
+        float targetLoad = 0.8f / loadingDataQuantity;
+
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync((int)scene, LoadSceneMode.Additive);
+
+
+        //Load Map
+        while (!loadOperation.isDone)
+        {
+            UpdateProgressBar(targetLoad);
+            yield return null;
+        }
+
+        barSpeed = fasterBarSpeed;
+        while (loadingSlider.value < targetLoad)
+        {
+            UpdateProgressBar(targetLoad);
+            yield return null;
+        }
+        targetLoad += targetLoad;
+
+        //Load Parsing
+        yield return DataManager.Instance.CheckIsParseDone();
+
+        barSpeed = fasterBarSpeed;
+        while (loadingSlider.value < targetLoad)
+        {
+            UpdateProgressBar(targetLoad);
+            yield return null;
+        }
+        barSpeed = generalBarSpeed;
+        yield return new WaitForSeconds(spareTimeToLoad);
+        targetLoad += targetLoad;
+
+        // + Add Load
+        // player save point
+
+        // Finish Loading
+        while (loadingSlider.value < 1.0f)
+        {
+            UpdateProgressBar(1.0f);
+            yield return null;
+        }
+        yield return new WaitForSeconds(spareTimeToLoad);
+
+        loadingSlider.value = 0.0f;
+        TestCenterWithScene.Instance.FinishLoading();
+
+        SceneManager.UnloadSceneAsync((int)SceneName.LoadingScene);
+    }
+
+    private void UpdateProgressBar(float max)
+    {
+        if (loadingSlider.value < max)
+            loadingSlider.value += barSpeed;
     }
 }
