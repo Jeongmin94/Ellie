d0702e5523bbbce0a6a913514d6df10c417d7718
jeongmin <extraking@naver.com>
2023-11-29 21:56:44 +0900

    #ELLIE-118: 퀘스트 UI 추가

---
+++ Assets/Scripts/UI/Quest/QuestCanvasTestClient.cs
@@ -0,0 +1,58 @@
+using System;
+using Assets.Scripts.Managers;
+using Assets.Scripts.Utils;
+using Channels.Components;
+using Channels.Type;
+using Channels.UI;
+using UnityEngine;
+
+namespace Assets.Scripts.UI.Quest
+{
+    public class QuestCanvasTestClient : MonoBehaviour
+    {
+        private TicketMachine ticketMachine;
+
+        private void Awake()
+        {
+            ticketMachine = gameObject.GetOrAddComponent<TicketMachine>();
+            ticketMachine.AddTickets(ChannelType.UI);
+
+            TicketManager.Instance.Ticket(ticketMachine);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                UIPayload payload = new UIPayload();
+
+                payload.uiType = UIType.Notify;
+                payload.actionType = ActionType.ClearQuest;
+
+                ticketMachine.SendMessage(ChannelType.UI, payload);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                UIPayload payload = new UIPayload();
+
+                payload.uiType = UIType.Notify;
+                payload.actionType = ActionType.SetQuestName;
+                payload.questInfo = QuestInfo.Of(null, "Test", null);
+
+                ticketMachine.SendMessage(ChannelType.UI, payload);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                UIPayload payload = new UIPayload();
+
+                payload.uiType = UIType.Notify;
+                payload.actionType = ActionType.SetQuestDesc;
+                payload.questInfo = QuestInfo.Of(null, null, "TestDesc");
+
+                ticketMachine.SendMessage(ChannelType.UI, payload);
+            }
+        }
+    }
+}
