c3facef3f9cf6ee3dde54a9850b3007d61b7f23c
jeongmin <extraking@naver.com>
2023-11-30 20:01:40 +0900

    #ELLIE-124: 오프닝 UI 사운드 적용

--- Assets/Scripts/UI/Opening/PopupMenu/MenuButton/YesMenuButton.cs
+++ Assets/Scripts/UI/Opening/PopupMenu/MenuButton/YesMenuButton.cs
@@ -1,9 +1,14 @@
+using Assets.Scripts.Managers;
+
 namespace Assets.Scripts.UI.PopupMenu
 {
     public class YesMenuButton : MenuButton
     {
+        public static readonly string SoundOk = "click4";
+
         public override void Click()
         {
+            SoundManager.Instance.PlaySound(SoundManager.SoundType.UISfx, SoundOk);
             PopupPayload payload = new PopupPayload();
             payload.buttonType = ButtonType.Yes;
 
