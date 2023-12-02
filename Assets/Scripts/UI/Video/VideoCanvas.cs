6ab5a3cabfc30e770fd955a8bbea74af45df1e82
jeongmin <extraking@naver.com>
2023-12-02 16:20:28 +0900

    #ELLIE-137: 비디오 플레이어 추가 중

---
+++ Assets/Scripts/UI/Video/VideoCanvas.cs
@@ -0,0 +1,18 @@
+using Assets.Scripts.UI.Framework.Popup;
+
+namespace Assets.Scripts.UI.Video
+{
+    public class VideoCanvas : UIPopup
+    {
+        
+        private void Awake()
+        {
+            Init();
+        }
+
+        protected override void Init()
+        {
+            base.Init();
+        }
+    }
+}
