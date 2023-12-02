8e2c2e8006d3f3b3865b62d297ff3a1d62ea0a09
clairhardt98 <woojae0107@gmail.com>
2023-12-02 17:05:22 +0900

    #ELLIE-127 : 기타 아이템 파싱 분리 및 곡괭이 습득 로직 수정

---
+++ Assets/Scripts/Data/GoogleSheet/4400Etc/EtcDataParsingInfo.cs
@@ -0,0 +1,69 @@
+﻿using Assets.Scripts.Item;
+using Assets.Scripts.UI.Inventory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Data.GoogleSheet._4400Etc
+{
+    [Serializable]
+    public class EtcData : ItemMetaData
+    { }
+
+    [CreateAssetMenu(fileName = "EtcData", menuName = "GameData List/EtcData")]
+
+    public class EtcDataParsingInfo : DataParsingInfo
+    {
+        public List<EtcData> etcDatas = new List<EtcData>();
+
+        public override T GetIndexData<T>(int index)
+        {
+            if (typeof(T) == typeof(EtcData))
+            {
+                return etcDatas.Find(m => m.index == index) as T;
+            }
+            return default(T);
+        }
+
+        public override void Parse()
+        {
+            etcDatas.Clear();
+
+            string[] lines = tsv.Split('\n');
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (string.IsNullOrEmpty(lines[i])) continue;
+
+                string[] entries = lines[i].Split('\t');
+
+                EtcData data = new EtcData();
+
+                try
+                {
+                    data.groupType = GroupType.Etc;
+                    //인덱스
+                    data.index = int.Parse(entries[0].Trim());
+                    //이름
+                    data.name = entries[1].Trim();
+                    // 설명
+                    data.description = entries[2].Trim();
+
+                    // ui 이미지
+                    data.imageName = "UI/Item/Etc/" + entries[3].Trim();
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError($"Error parsing line {i + 1}: {entries[i]}");
+                    Debug.LogError(e);
+                    continue;
+                }
+
+                etcDatas.Add(data);
+            }
+        }
+    }
+}
