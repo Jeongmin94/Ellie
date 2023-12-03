d06013a7e387a30825e783b72d2ca417f771ab0a
DJMAXJOAA <109760354+DJMAXJOAA@users.noreply.github.com>
2023-12-03 17:48:45 +0900

    # ELLIE-210 : 보스 사운드 작업

--- Assets/Scripts/Boss1/DataScripts/Terrapupa/TerrapupaSpinningAttackData.cs
+++ Assets/Scripts/Boss1/DataScripts/Terrapupa/TerrapupaSpinningAttackData.cs
@@ -1,5 +1,6 @@
 ﻿using Assets.Scripts.StatusEffects;
 using Channels.Combat;
+using Sirenix.OdinInspector;
 using System.Collections;
 using TheKiwiCoder;
 using UnityEngine;
@@ -12,22 +13,26 @@ public class TerrapupaSpinningAttackData : BaseBTData
         dataName = "TerrapupaSpinningAttack";
     }
 
-    [Header("이펙트 설정")]
-    [Tooltip("회전 공격 시 이펙트")] public GameObject spinningAttackEffect1;
+    [Title("사운드 설정")]
+    [InfoBox("회전공격 공격 사운드")] public string sound1 = "TerrapupaNormalAttack";
 
-    [Header("회전 공격")]
-    [Tooltip("플레이어에게 타겟팅 회전 속도")] public float rotationSpeed = 2.0f;
-    [Tooltip("회전 공격 중 이동 속도")] public float movementSpeed = 1.0f;
-    [Tooltip("회전 공격 공격 적중 거리")] public float effectiveRadius = 7.0f;
+    [Title("이펙트 설정")]
+    [InfoBox("회전 공격 시 이펙트")] public GameObject spinningAttackEffect1;
 
-    [Header("피격 정보")]
-    [Tooltip("공격력")] public int attackValue = 5;
-    [Tooltip("상태 이상")] public StatusEffectName statusEffect = StatusEffectName.WeakRigidity;
-    [Tooltip("상태 이상 지속시간")] public float statusEffectDuration = 0.2f;
-    [Tooltip("상태 이상 힘(force)")] public float statusEffectForce = 0.0f;
+    [Title("회전 공격")]
+    [InfoBox("플레이어에게 타겟팅 회전 속도")] public float rotationSpeed = 2.0f;
+    [InfoBox("회전 공격 중 이동 속도")] public float movementSpeed = 1.0f;
+    [InfoBox("회전 공격 공격 적중 거리")] public float effectiveRadius = 7.0f;
+
+    [Title("피격 정보")]
+    [InfoBox("공격력")] public int attackValue = 5;
+    [InfoBox("상태 이상")] public StatusEffectName statusEffect = StatusEffectName.WeakRigidity;
+    [InfoBox("상태 이상 지속시간")] public float statusEffectDuration = 0.2f;
+    [InfoBox("상태 이상 힘(force)")] public float statusEffectForce = 0.0f;
 
     public override void Init(BehaviourTree tree)
     {
+        SetBlackboardValue<string>("sound1", sound1, tree);
         SetBlackboardValue<GameObject>("effect1", spinningAttackEffect1, tree);
         SetBlackboardValue<float>("rotationSpeed", rotationSpeed, tree);
         SetBlackboardValue<float>("movementSpeed", movementSpeed, tree);
