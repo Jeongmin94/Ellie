78c466ea1418e49d692c4e614dc054628e3f9a2e
Lewis Hahn <iamonedeo@gmail.com>
2023-12-15 14:26:55 +0900

    #ELLIE-196 이걸 까먹네

--- Assets/Scripts/Monsters/EffectStatus/StatusEffectConcreteStrategies/MonsterStatusEffectFreezing.cs
+++ Assets/Scripts/Monsters/EffectStatus/StatusEffectConcreteStrategies/MonsterStatusEffectFreezing.cs
@@ -23,7 +23,7 @@ namespace Assets.Scripts.Monsters.EffectStatus.StatusEffectConcreteStrategies
             controller.AddStatusEffect(this);
             StartFreeze();
 
-            yield return new WaitForSeconds(5.0f);
+            yield return new WaitForSeconds(duration);
 
             controller.RemoveStatusEffect(this);
             EndFreeze();
